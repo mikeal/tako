6c2f1d1ef50e77f986bab85f797f91e9a6761443
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-03-06 11:32:39 +0100

    Add test for routes and some common functions

---
+++ tests/common.js
@@ -0,0 +1,18 @@
+var assert = require('assert')
+
+exports.PORT = 6000
+exports.URL = 'http://localhost:' + exports.PORT
+
+exports.all = function (functions, cb) {
+  var done = functions.length
+  functions.forEach(function (f) {
+    f[0](function () {
+      f[1].apply(this, arguments)
+      if (--done === 0) return cb()
+    })
+  })
+}
+
+exports.assertStatus = function (res, status) {
+  assert.equal(res.statusCode, status)
+}
