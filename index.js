86cc79b0a158c59f7baaa647a6fe7ae22649c231
Saam Barati <saambarati1@gmail.com>
2012-05-08 13:41:15 -0700

    reworked ctor for Maple

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var util = require('util')
   , http = require('http')
   , https = require('https')
   // Dependencies
-  , maple = require('Maple')
+  , Maple = require('Maple')
   , filed = require('filed')
   // Local Imports
   , handlebars = require('./handlebars')
@@ -360,7 +360,7 @@ function Application (options) {
     }
   }
 
-  self.router = new maple.RouteTree()
+  self.router = new Maple()
   self.on('newroute', function (route) {
     self.router.define(route.path, function (req, resp, authHandler){
       route.handler(req, resp, authHandler)
