f601fc43aafba4f2c9a4b29cf58c95484bdbe6b2
Tobias Kopelke <nox@sao.bavhaus.de>
2012-08-02 12:13:29 +0200

    fixed no content type for put & post

--- index.js
+++ index.js
@@ -567,7 +567,7 @@ function JSONRequestHandler (req, resp) {
     return orig.call(resp, chunk)
   }
   if (req.method === "PUT" || req.method === "POST") {
-    if (req.headers['content-type'].split(';')[0] === 'application/json') {
+    if (req.headers['content-type'] && (req.headers['content-type'].split(';')[0] === 'application/json')) {
       req.on('body', function (body) {
         try {
           req.emit('json', JSON.parse(body));
